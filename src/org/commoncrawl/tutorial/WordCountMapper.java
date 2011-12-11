42d004251cc3a5ab3bdbf637be519b2eec8c37c6
Steve Salevan <steve.salevan@gmail.com>
2011-12-11 12:46:47 -0800

    tuning job

--- src/org/commoncrawl/tutorial/WordCountMapper.java
+++ src/org/commoncrawl/tutorial/WordCountMapper.java
@@ -26,8 +26,8 @@ public class WordCountMapper extends MapReduceBase
 	  page_content.replaceAll("\\t|\\n", " ");
 	  page_content.replaceAll("\\s+", " ");
 	  // Split by space and output to OutputCollector
-	  output.collect(new Text("test"), new LongWritable(1));
-	  output.collect(new Text(page_content), new LongWritable(1));
+	  //output.collect(new Text("test"), new LongWritable(1));
+	  output.collect(new Text(value.getUri()), new LongWritable(1));
 	  //for (String word: page_content.split(" ")) {
 		  //output.collect(new Text("test"), new LongWritable(1));
 		  //output.collect(new Text(word), new LongWritable(1));
