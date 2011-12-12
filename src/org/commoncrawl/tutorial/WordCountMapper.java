965e38c10e19a98017c6530bd86c264413c73244
Steve Salevan <steve.salevan@gmail.com>
2011-12-11 17:39:56 -0800

    tuning job

--- src/org/commoncrawl/tutorial/WordCountMapper.java
+++ src/org/commoncrawl/tutorial/WordCountMapper.java
@@ -1,6 +1,10 @@
 package org.commoncrawl.tutorial;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.Scanner;
 import java.util.zip.GZIPInputStream;
 
@@ -19,10 +23,17 @@ public class WordCountMapper extends MapReduceBase
   public void map(Text key, ArcFileItem value,
 	      OutputCollector<Text, LongWritable> output, Reporter reporter)
 	      throws IOException {
-	  try {
+	  //try {
 		// Un-GZIP compressed page data
-	    GZIPInputStream gzipIn = new GZIPInputStream(value.getContent().getBytes());
-	    String page_content = new Scanner(gzipIn).useDelimiter("\\A").next();
+		ByteArrayInputStream inputStream = new ByteArrayInputStream(
+				value.getContent().getReadOnlyBytes(), 0, value.getContent().getCount());
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(inputStream,Charset.forName("ASCII")));
+		String page_content = "";
+		while ((page_content += reader.readLine()) != null) {}
+		  //GZIPInputStream gzipIn = new GZIPInputStream(value.getContent().getBytes());
+	    
+	    //String page_content = new Scanner(inputStream).useDelimiter("\\A").next();
         //String page_content = Jsoup.parse(value.getContent().toString()).text();
   	    // Remove all punctuation
 	    page_content.replaceAll("\\p{Punct}", "");
@@ -30,14 +41,11 @@ public class WordCountMapper extends MapReduceBase
 	    page_content.replaceAll("\\t|\\n", " ");
 	    page_content.replaceAll("\\s+", " ");
 	    // Split by space and output to OutputCollector
-	    //output.collect(new Text("test"), new LongWritable(1));
-	    //output.collect(new Text(value.getUri()), new LongWritable(1));
 	    for (String word: page_content.split(" ")) {
-		  //output.collect(new Text("test"), new LongWritable(1));
 		  output.collect(new Text(word), new LongWritable(1));
 	    }
-	  } catch (IOException e) {
-		  return;  // not in GZIP format, we can't process it
-	  }
+	  //} catch (IOException e) {
+      //  return;  // not in GZIP format, we can't process it
+	  //}
   }
 }
