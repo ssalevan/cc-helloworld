053fe089866ea6ebb04c66122c8fae6398de6e5b
Steve Salevan <steve.salevan@gmail.com>
2011-12-11 18:32:08 -0800

    tuning job

--- src/org/commoncrawl/tutorial/WordCountMapper.java
+++ src/org/commoncrawl/tutorial/WordCountMapper.java
@@ -15,7 +15,7 @@ import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
 import org.commoncrawl.protocol.shared.ArcFileItem;
-//import org.jsoup.Jsoup;
+import org.jsoup.Jsoup;
 
 public class WordCountMapper extends MapReduceBase 
   implements Mapper<Text, ArcFileItem, Text, LongWritable> {
@@ -23,31 +23,21 @@ public class WordCountMapper extends MapReduceBase
   public void map(Text key, ArcFileItem value,
       OutputCollector<Text, LongWritable> output, Reporter reporter)
       throws IOException {
-	  //try {
-		// Un-GZIP compressed page data
-		ByteArrayInputStream inputStream = new ByteArrayInputStream(
-            value.getContent().getReadOnlyBytes(), 0, value.getContent().getCount());
-	    String page_content = new Scanner(inputStream).useDelimiter("\\A").next();
-		/*BufferedReader reader = new BufferedReader(
-            new InputStreamReader(inputStream,Charset.forName("ASCII")));
-		String page_content = "";*/
-		
-		//while ((page_content += reader.readLine()) != null) {}
-		//GZIPInputStream gzipIn = new GZIPInputStream(value.getContent().getBytes());
-	    
-	    //String page_content = new Scanner(inputStream).useDelimiter("\\A").next();
-        //String page_content = Jsoup.parse(value.getContent().toString()).text();
-  	    // Remove all punctuation
-	    page_content.replaceAll("\\p{Punct}", "");
-	    // Normalize whitespace to single spaces
-	    page_content.replaceAll("\\t|\\n", " ");
-	    page_content.replaceAll("\\s+", " ");
-	    // Split by space and output to OutputCollector
-	    for (String word: page_content.split(" ")) {
-		  output.collect(new Text(word), new LongWritable(1));
-	    }
-	  //} catch (IOException e) {
-      //  return;  // not in GZIP format, we can't process it
-	  //}
+	// Un-GZIP compressed page data
+	ByteArrayInputStream inputStream = new ByteArrayInputStream(
+        value.getContent().getReadOnlyBytes(), 0,
+        value.getContent().getCount());
+    String content = new Scanner(inputStream)
+        .useDelimiter("\\A").next();
+    String page_text = Jsoup.parse(value.getContent().toString()).text();
+    // Remove all punctuation
+    page_text.replaceAll("\\p{Punct}", "");
+    // Normalize whitespace to single spaces
+    page_text.replaceAll("\\t|\\n", " ");
+    page_text.replaceAll("\\s+", " ");
+    // Split by space and output to OutputCollector
+    for (String word: page_text.split(" ")) {
+      output.collect(new Text(word), new LongWritable(1));
+    }
   }
 }
